532acd68d8acd4b98a0713c2177b78a228101449
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2024-11-20 18:39:41 +0500

    Class added to get destination address of user for fund transfer

---
+++ src/utils/destination.js
@@ -0,0 +1,39 @@
+class Destination {
+    constructor(confidentialAddress,address,blindingPubkey,scriptPubkey){
+        this.confidentialAddress=confidentialAddress;
+        this.address=address;
+        this.blindingPubkey=blindingPubkey;
+        this.scriptPubkey=scriptPubkey;
+    }
+    static async create(confidentialAddress) {
+        const wally = await import("wallycore");
+        const blindingPubkey = wally.confidential_addr_to_ec_public_key(confidentialAddress, wally.WALLY_CA_PREFIX_LIQUID_REGTEST);
+        const address = wally.confidential_addr_to_addr(confidentialAddress, wally.WALLY_CA_PREFIX_LIQUID_REGTEST);
+        let scriptPubKey = Buffer.alloc(wally.WALLY_SCRIPTPUBKEY_P2PKH_LEN);
+        scriptPubKey = wally.address_to_scriptpubkey(
+          address,
+          wally.WALLY_NETWORK_LIQUID_REGTEST
+        );
+        return new Destination(
+            confidentialAddress,
+            address,
+            blindingPubkey,
+            scriptPubKey
+        )
+    }
+    getConfidentialAddress() {
+        return this.confidentialAddress;
+    }
+
+    getBlindingPubkey() {
+        return this.blindingPubkey;
+    }
+
+    getAddress() {
+        return this.address;
+    }
+
+    getScriptPubkey() {
+        return this.scriptPubkey;
+    }
+}
